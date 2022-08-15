27bb762bd92a8d48f81ad6eab98a291f3b722c38
filip13131 <f.kaminski2000@gmail.com>
2022-08-15 15:23:08 +0200

    init test

---
+++ src/test/java/org/filip13131/neptunesPrideHelper/apiCommunicator/IronhelmetCommunicatorTest.java
@@ -0,0 +1,13 @@
+package org.filip13131.neptunesPrideHelper.apiCommunicator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class IronhelmetCommunicatorTest {
+    @Test
+    public void randomTest(){
+        assertEquals("x","x");
+    }
+}
